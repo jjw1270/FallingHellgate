b903971ddd60f1da93fdb3db565d5e0af14ae41c
Yunje Jang <jjw1270@naver.com>
2023-07-28 16:12:26 +0900

    Add Item Infomation Base
    ItemBase <-- ConsumableItemBase, EquipmentItemBase

---
+++ Source/FallingHellgate/Public/Item/Equipment/EquipmentItemBase.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemBase.h"
+#include "EquipmentItemBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FALLINGHELLGATE_API UEquipmentItemBase : public UItemBase
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Infomation")
+	int32 Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Infomation")
+	int32 Stamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Infomation")
+	int32 AttackPower;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Infomation")
+	int32 AttackSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Infomation")
+	int32 CriticalChance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Infomation")
+	int32 DefensivePower;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetHealth() { return Health; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetStamina() { return Stamina; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetAttackPower() { return AttackPower; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetAttackSpeed() { return AttackSpeed; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetCriticalChance() { return CriticalChance; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetDefensivePower() { return DefensivePower; }
+
+};
